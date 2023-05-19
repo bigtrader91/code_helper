0b16e947942d2a665ae6bcc09ecfe7b5c898066d
bigtrader91 <dahy949@gmail.com>
2023-05-19 13:15:39 +0000

    change prompt.py

--- code_helper.py
+++ code_helper.py
@@ -3,9 +3,6 @@ import streamlit as st
 import openai
 import os
 openai.api_key = st.secrets['OPENAI_API_KEY']
-# openai.api_key = os.getenv("OPENAI_API_KEY")
-
-
 
 
 # GPT-3 API를 호출하여 변수명을 가져오는 함수를 정의합니다.
@@ -31,6 +28,38 @@ st.title('Python Variable Name Generator')
 # 사용자 입력 인터페이스를 만듭니다.
 description = st.text_input('Describe the variable you need a name for:')
 
+
+
+# GPT-3 API를 호출하여 변수명을 가져오는 함수를 정의합니다.
+def get_variable_name(prompt, model_name):
+    try:
+        response = openai.Completion.create(
+            model=model_name,
+            prompt=prompt,
+            temperature=0,
+            max_tokens=60,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0,
+            n=3
+        )
+        return response['choices'][0]['text'].strip()
+    except Exception as e:
+        return f"An error occurred: {e}"
+
+# Streamlit 앱을 설정합니다.
+st.title('Python Variable Name Generator')
+
+# 사용자가 모델을 선택할 수 있는 드롭다운 메뉴를 생성합니다.
+model_name = st.selectbox(
+    'Select a model:',
+    ('ada', 'curie', 'babbage', 'davinci')
+)
+
+# 사용자 입력 인터페이스를 만듭니다.
+description = st.text_input('Describe the variable you need a name for:')
+
+# 원하는 프롬프트를 작성합니다.
 prompt=f'''
 I want to make class names, functions, and variable names in Python according to the following rules. Please listen to my explanation and write a variable name according to the rules.
 
@@ -44,14 +73,15 @@ Please suggest a total of 3 names like the example and answer them in a good way
 {description}
 [Example]
 \n
-1. abc \n
-2. defer \n
-3. fet \n
+1. aaa \n
+2. bbb \n
+3. ccc \n
 
 Please suggest a total of 3 names as in the example.
 '''
+
 # 사용자가 설명을 입력하면 함수를 호출합니다.
 if description:
     with st.spinner('Waiting for ChatGPT...'):
-        variable_name = get_variable_name(prompt)
+        variable_name = get_variable_name(prompt, model_name)
         st.write(variable_name)
