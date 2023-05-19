03ba295696bfa50c60ade9a5b0a81d284e2deead
bigtrader91 <88607278+bigtrader91@users.noreply.github.com>
2023-05-19 21:43:09 +0900

    Update code_helper.py

--- code_helper.py
+++ code_helper.py
@@ -5,9 +5,22 @@ import os
 openai.api_key = st.secrets['OPENAI_API_KEY']
 # openai.api_key = os.getenv("OPENAI_API_KEY")
 
-prompt='''
+prompt=f'''
+I want to make class names, functions, and variable names in Python according to the following rules. Please listen to my explanation and write a variable name according to the rules.
+
+Rule 1: In the case of df, dict, and list, you must write it so that it is placed at the front of the variable.
+Rule 2: If the name of a variable is lengthened, use _
+Rule 3: Use nouns as much as you can.
+
+Please follow the most basic Python naming convention for the parts that are not set in the rules.
 
 
+[description]
+{}
+[Example]
+1.
+2.
+3.
 
 '''
 
@@ -37,5 +50,5 @@ description = st.text_input('Describe the variable you need a name for:')
 # 사용자가 설명을 입력하면 함수를 호출합니다.
 if description:
     with st.spinner('Waiting for ChatGPT...'):
-        variable_name = get_variable_name("I need a Python variable name for:", description)
+        variable_name = get_variable_name(prompt, description)
         st.write(f"Suggested variable name: `{variable_name}`")
